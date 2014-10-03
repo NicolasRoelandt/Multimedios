1c7a13839d8079140f2f967739d3e55a26e2a250
Nicolas Roelandt <nicolasroelandt@Nicolass-MacBook-Pro-2.local>
2014-10-03 20:10:42 -0300

    - prueba

--- src/com/example/diccperso/Registro.java
+++ src/com/example/diccperso/Registro.java
@@ -82,6 +82,7 @@ public class Registro extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
+		int p = 9;
 		if (id == R.id.action_settings) {
 			return true;
 		}
